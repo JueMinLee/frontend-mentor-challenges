bc21e2cbe7bfd3f7a36b30b605ae1add1ce1ae21
JM <46175651+JueMinLee@users.noreply.github.com>
2020-06-24 12:55:25 +0800

    fix(index): Fix bug and refactor
    - Fix when current index is -1 not exit the function
    - Refactor script.js with community's feedback
    - Fix HTML issue. Change index.html `section` to `div` due to no heading
    - Fix CSS styling
    
    Feedback: https://www.frontendmentor.io/solutions/vanilla-html-css-javascript-oIH8HWPW4

--- coding-bootcamp-testimonials-slider/script.js
+++ coding-bootcamp-testimonials-slider/script.js
@@ -1,62 +1,54 @@
 const switchButtons = document.querySelectorAll('[data-switch]')
-const images = document.querySelectorAll('[data-image]')
-const quotes = document.querySelectorAll('[data-quote]')
+const images = [...document.querySelectorAll('[data-image]')]
+const quotes = [...document.querySelectorAll('[data-quote]')]
+const totalImages = images.length
+const totalQuotes = quotes.length
+let currentImageIndex = images.findIndex(
+  image => !image.classList.contains('hidden')
+)
+let currentQuoteIndex = quotes.findIndex(
+  quote => !quote.classList.contains('hidden')
+)
 
-switchButtons.forEach((button) => {
+switchButtons.forEach(button =>
   button.addEventListener('click', switchTestimonial)
-})
+)
 
 function switchTestimonial(e) {
-  const selection = e.currentTarget.dataset.switch
-  const totalImages = images.length
-  const totalQuotes = quotes.length
-  let currentImageIndex = getCurrentIndex(images)
-  let currentQuoteIndex = getCurrentIndex(quotes)
+  const selection = e.currentTarget.dataset.switch // OR this.dataset.switch
+  let newImageIndex = 0
+  let newQuoteIndex = 0
 
   if (
-    selection !== 'next' &&
-    selection !== 'prev' &&
-    currentImageIndex !== -1 &&
-    currentQuoteIndex !== -1
+    (selection !== 'next' && selection !== 'prev') ||
+    currentImageIndex === -1 ||
+    currentQuoteIndex === -1
   )
     return
+
   if (selection === 'next') {
-    const nextImageIndex = currentImageIndex + 1
-    const nextQuoteIndex = currentQuoteIndex + 1
-
-    if (nextImageIndex < totalImages) {
-      switchImage(currentImageIndex, nextImageIndex)
-      switchQuote(currentQuoteIndex, nextQuoteIndex)
-      return
-    }
-
-    switchImage(currentImageIndex, 0)
-    switchQuote(currentQuoteIndex, 0)
+    newImageIndex = getNextIndex(currentImageIndex, totalImages)
+    newQuoteIndex = getNextIndex(currentQuoteIndex, totalQuotes)
   } else {
-    const previousImageIndex = currentImageIndex - 1
-    const nextQuoteIndex = currentQuoteIndex - 1
-
-    if (previousImageIndex >= 0) {
-      switchImage(currentImageIndex, previousImageIndex)
-      switchQuote(currentQuoteIndex, nextQuoteIndex)
-      return
-    }
-
-    switchImage(currentImageIndex, totalImages - 1)
-    switchQuote(currentQuoteIndex, totalQuotes - 1)
+    newImageIndex = getPrevIndex(currentImageIndex, totalImages)
+    newQuoteIndex = getPrevIndex(currentQuoteIndex, totalQuotes)
   }
+
+  hideCurrentShowNext(images, currentImageIndex, newImageIndex)
+  hideCurrentShowNext(quotes, currentQuoteIndex, newQuoteIndex)
+  currentImageIndex = newImageIndex
+  currentQuoteIndex = newQuoteIndex
 }
 
-function getCurrentIndex(list = [], keyword = 'hidden') {
-  return Array.from(list).findIndex((item) => !item.classList.contains(keyword))
+function getNextIndex(currentIndex, totalItems) {
+  return (currentIndex + 1) % totalItems
 }
 
-function switchImage(currentIndex = 0, nextIndex = 1) {
-  images[currentIndex].classList.toggle('hidden')
-  images[nextIndex].classList.toggle('hidden')
+function getPrevIndex(currentIndex, totalItems) {
+  return (currentIndex + totalItems - 1) % totalItems
 }
 
-function switchQuote(currentIndex = 0, nextIndex = 1) {
-  quotes[currentIndex].classList.toggle('hidden')
-  quotes[nextIndex].classList.toggle('hidden')
+function hideCurrentShowNext(list, currentIndex, nextIndex) {
+  list[currentIndex].classList.toggle('hidden')
+  list[nextIndex].classList.toggle('hidden')
 }
